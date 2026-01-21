3f25e60fa8952573ccd076522069dd7b1da772b5
ankushgarg44 <146913479+ankushgarg44@users.noreply.github.com>
2026-01-21 11:59:17 +0530

    hello

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import Home from './components/Home.jsx'
-function App () {
+import React from "react";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  const token = localStorage.getItem("jwt");
   return (
     <div>
-      <Home/>
-
+      <Routes>
+        <Route path="/"element={<Home /> }/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
